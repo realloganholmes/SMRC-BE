b1a7a7e87512535a7326586c12b4ef1d587da192
Holmes <logan.holmes@kroger.com>
2025-04-22 11:24:45 -0400

    Add recaps

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const authRoutes = require('./routes/auth');
 const coolerRoutes = require('./routes/coolerRoutes');
 const coolerAdminRoutes = require('./routes/coolerAdminRoutes');
+const recapRoutes = require('./routes/recapRoutes');
+const recapAdminRoutes = require('./routes/recapAdminRoutes');
+const uploadRouter = require('./routes/uploadFileRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const User = require('./models/User');
 
@@ -66,6 +69,29 @@ const protectCoolerAdmin = async (req, res, next) => {
   }
 };
 
+const protectRecapAdmin = async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Not authorized' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not found.' });
+    }
+
+    if (!user.admin && !user.recapAdmin) {
+      return res.status(401).json({ message: 'User is not an administrator' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
 const protect = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'Not authorized' });
@@ -91,6 +117,12 @@ app.use('/api/coolers', protect, coolerRoutes);
 
 app.use('/api/coolers-admin', protectCoolerAdmin, coolerAdminRoutes);
 
+app.use('/api/recaps', protect, recapRoutes);
+
+app.use('/api/recaps-admin', protectRecapAdmin, recapAdminRoutes);
+
+app.use('/api/uploadFile', uploadRouter);
+
 app.use('/api/admin', protectAdmin, adminRoutes);
 
-app.listen(8080, () => console.log('Server running on port 5000'));
+app.listen(8080, () => console.log('Server running on port 8080'));
